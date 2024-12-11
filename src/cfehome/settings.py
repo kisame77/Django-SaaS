354afdb0454545eb122e875ae30dc3bfbc8a91fc
aurexus <aurexus@localhost.localdomain>
2024-12-11 16:09:52 +0530

    Prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k2_d3a5rf=yzk4ek0nl!aqm8wtdd-f0al#b$vvc@hzq(#34yk2"
+SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = [
-    ".railway.app",
-    "django-saas-production-713f.up.railway.app"
-]
+print("DJANGO_SECRET_KEY", SECRET_KEY)
+print("DEBUG", DEBUG)
+
+ALLOWED_HOSTS = [".railway.app", "django-saas-production-713f.up.railway.app"]
 
 if DEBUG:
     ALLOWED_HOSTS += [
@@ -92,6 +93,19 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        ),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
